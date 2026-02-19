f29134d1882b2373ed9da83d98e225a359841b99
heyrapto <kalejaiyecaleb@gmail.com>
2026-02-20 00:12:13 +0100

    feat: changes

--- app/omni-farming/page.tsx
+++ app/omni-farming/page.tsx
@@ -6,16 +6,16 @@ import { TransactionPanel } from "@/app/components/omni-farming/TransactionPanel
 
 export default function OmniFarmingPage() {
     return (
-        <div className="flex flex-col lg:flex-row gap-6 w-full">
-            {/* Left Column: Yield Selection, Stats, and Charts */}
-            <div className="flex-1 flex flex-col min-w-0">
-                <YieldSelector />
-                <YieldStatsCard />
-                <ChartsArea />
-            </div>
+        <>
+            <YieldSelector />
+            <div className="flex flex-col lg:flex-row gap-6 w-full">
+                <div className="flex-1 flex flex-col min-w-0">
 
-            {/* Right Column: Deposit/Withdraw, Positions, Support */}
-            <TransactionPanel />
-        </div>
+                    <YieldStatsCard />
+                    <ChartsArea />
+                </div>
+                <TransactionPanel />
+            </div>
+        </>
     );
 }
